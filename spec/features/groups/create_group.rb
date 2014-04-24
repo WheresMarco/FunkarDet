e686d703747e81ffa58a543f06c57247a1b1baca
Marco Hyyryläinen <marco@wheresmar.co>
2014-04-24 21:54:43 +0200

    Refaktorerade grupp-skapande i spec

--- spec/features/groups/create_group.rb
+++ spec/features/groups/create_group.rb
@@ -1,36 +1,31 @@
 require 'spec_helper'
 
 describe "Creating groups" do
-  it "redirects to the group list index page on success" do
+  def create_group(options={})
+    options[:name] ||= "Testgroup"
+
     visit "/groups"
     click_link "New Group"
     expect(page).to have_content("New group")
 
-    fill_in "Name", with: "Testgroup"
+    fill_in "Name", with: options[:name]
 
     select "2013", from: "group[creationDate(1i)]"
     select "February", from: "group[creationDate(2i)]"
     select "26", from: "group[creationDate(3i)]"
 
     click_button "Create Group"
+  end
 
+  it "redirects to the group list index page on success" do
+    create_group
     expect(page).to have_content("Testgroup")
   end
 
   it "displays an error when the group has no name" do
     expect(Group.count).to eq(0)
 
-    visit "/groups"
-    click_link "New Group"
-    expect(page).to have_content("New group")
-
-    fill_in "Name", with: ""
-
-    select "2013", from: "group[creationDate(1i)]"
-    select "February", from: "group[creationDate(2i)]"
-    select "26", from: "group[creationDate(3i)]"
-
-    click_button "Create Group"
+    create_group name: ""
 
     expect(page).to have_content("error")
     expect(Group.count).to eq(0)
@@ -42,17 +37,7 @@ describe "Creating groups" do
   it "displays an error when the group has a name less then 3 characters" do
     expect(Group.count).to eq(0)
 
-    visit "/groups"
-    click_link "New Group"
-    expect(page).to have_content("New group")
-
-    fill_in "Name", with: "Hi"
-
-    select "2013", from: "group[creationDate(1i)]"
-    select "February", from: "group[creationDate(2i)]"
-    select "26", from: "group[creationDate(3i)]"
-
-    click_button "Create Group"
+    create_group name: "Hi"
 
     expect(page).to have_content("error")
     expect(Group.count).to eq(0)
