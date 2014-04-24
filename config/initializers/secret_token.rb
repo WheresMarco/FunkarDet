ee0dda5a8194b065046ba165e8a2c908d0820813
Marco Hyyryläinen <marco@wheresmar.co>
2014-04-24 09:19:06 +0200

    Genererade en RoR-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funkardet::Application.config.secret_key_base = '6622486019e478e64f95cfd794707a44ba808f34b0bd7548a5c0d4d2f22b67d50ec1bf8871473b3acb5ec7f4379d3a53ec8248b42aa36e2de6a35cbfe1d3cf93'
--- readme.md
+++
@@ -1,4 +0,0 @@
-# Här kommer snart ett underbart projekt
-
-Under tiden, en kattbild:
-![naww](http://distilleryimage5.ak.instagram.com/a8cd0e46bb5811e3a14a126ca6d56785_8.jpg)
