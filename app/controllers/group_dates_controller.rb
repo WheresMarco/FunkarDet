0e726cc9ca1cd2abba436f06316a38eb48979cb9
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-07 12:25:42 +0200

    Skrev tester för group dates

--- app/controllers/group_dates_controller.rb
+++ app/controllers/group_dates_controller.rb
@@ -1,4 +1,5 @@
 class GroupDatesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
   end
 end
--- spec/features/group_dates/index_spec.rb
+++ spec/features/group_dates/index_spec.rb
@@ -3,13 +3,36 @@ require 'spec_helper'
 describe "Viewing group dates" do
   let!(:group) { group = Group.create(name: "Testgroup", creationDate: "2012-02-01") }
 
-  it "displays no items when a group date list is empty" do
+  def visit_group(group)
     visit "/groups"
-
     within "#group_#{group.id}" do
       click_link "List Dates"
     end
-    
-    expect(page).to have_content("GroupDates#index")
+  end
+
+  it "displays the name of the group" do
+    visit_group(group)
+    within("h1") do
+      expect(page).to have_content(group.name)
+    end
+  end
+
+  it "displays no items when a group date list is empty" do
+    visit_group(group)
+    expect(page.all("ul.group_dates li").size).to eq(0)
+  end
+
+  it "displays date content when a group has dates" do
+    group.group_dates.create(name: "First of may", from_date: "2014-05-01", to_date: "2014-05-02", description: "A nice sample text.")
+    group.group_dates.create(name: "Last of day", from_date: "2014-12-31", to_date: "2015-01-01", description: "A new sample text.")
+
+    visit_group(group)
+
+    expect(page.all("ul.group_dates li").size).to eq(2)
+
+    within "ul.group_dates" do
+      expect(page).to have_content("First of may")
+      expect(page).to have_content("Last of day")
+    end
   end
 end
