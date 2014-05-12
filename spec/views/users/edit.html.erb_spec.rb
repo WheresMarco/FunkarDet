c71363d2c10dc89522836dcf137e3a0d8416475d
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-12 12:43:15 +0200

    Fixade att testerna fungerade igen

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -5,6 +5,8 @@ describe "users/edit" do
     @user = assign(:user, stub_model(User,
       :username => "MyString",
       :email => "MyString",
+      :password => "MyString",
+      :password_confirmation => "MyString",
       :password_digest => "MyString"
     ))
   end
@@ -16,7 +18,8 @@ describe "users/edit" do
     assert_select "form[action=?][method=?]", user_path(@user), "post" do
       assert_select "input#user_username[name=?]", "user[username]"
       assert_select "input#user_email[name=?]", "user[email]"
-      assert_select "input#user_password_digest[name=?]", "user[password_digest]"
+      assert_select "input#user_password[name=?]", "user[password]"
+      assert_select "input#user_password_confirmation[name=?]", "user[password_confirmation]"
     end
   end
 end
