01ab51c8d8477503e75412bf541a390f97dcc604
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-13 19:00:49 +0200

    Ändrade testet för att göra att det fungera med existerande data

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "Signing up" do
   it "allows a user to sign up for the site and creates the object in the database" do
-    expect(User.count).to eq(0)
+    number_of_users = User.count
 
     visit "/"
     expect(page).to have_content("Sign Up")
@@ -14,6 +14,6 @@ describe "Signing up" do
     fill_in "Password (again)", with: "treehouse1234"
     click_button "Sign Up"
 
-    expect(User.count).to eq(1)
+    expect(User.count).to eq(number_of_users + 1)
   end
 end
