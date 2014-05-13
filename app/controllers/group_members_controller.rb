1c9fedbac79e314a0a0baf91c23a018910bd91e7
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-13 21:21:32 +0200

    Nu kan man se medlemmarna i gruppen

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,4 +1,16 @@
 class GroupMembersController < ApplicationController
+  before_action :require_user
+  before_action :find_group
+  
   def index
+  end
+
+  private
+  def find_group
+    @group = current_user.groups.find(params[:group_id])
+  end
+
+  def group_date_params
+    params[:group_date].permit(:name, :place)
   end
 end
