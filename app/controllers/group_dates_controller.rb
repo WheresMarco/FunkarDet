0847084e1906c94ee5a764dd1aecbdf6be4c4c33
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-24 17:21:40 +0200

    Changed some redirects

--- app/controllers/group_dates_controller.rb
+++ app/controllers/group_dates_controller.rb
@@ -14,7 +14,7 @@ class GroupDatesController < ApplicationController
 
     if @group_date.save
       flash[:success] = "Added group date."
-      redirect_to group_group_dates_path
+      redirect_to groups_path
     else
       flash[:error] = "Threre was a problem adding that group date."
       render action: :new
