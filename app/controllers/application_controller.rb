feaacfbeb190191ae487427af7e0aae4d69b71c5
Marco Hyyryläinen <marco@wheresmar.co>
2014-05-25 13:36:58 +0200

    Started I18N:ing my app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :success
+  before_action :set_locale
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,5 +15,14 @@ class ApplicationController < ActionController::Base
     else
       redirect_to new_user_session_path, notice: "You must be logged in to access that page."
     end
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,20 @@
 Funkardet::Application.routes.draw do
-  get "/login" => "user_sessions#new", as: :login
-  get "/logout" => "user_sessions#destroy", as: :logout
-  get "/login/select_user" => "user_sessions#select_user", as: :select_user
-  post "/login/select_user" => "user_sessions#select_user_post", as: :select_user_post
+  scope "(:locale)", locale: /en|se/ do
+    get "/login" => "user_sessions#new", as: :login
+    get "/logout" => "user_sessions#destroy", as: :logout
+    get "/login/select_user" => "user_sessions#select_user", as: :select_user
+    post "/login/select_user" => "user_sessions#select_user_post", as: :select_user_post
 
-  resources :users
-  resources :user_sessions, only: [:new, :create]
+    resources :users
+    resources :user_sessions, only: [:new, :create]
 
-  resources :groups do
-    resources :group_dates do
-      post :attend_date
+    resources :groups do
+      resources :group_dates do
+        post :attend_date
+      end
+      resources :group_members
     end
-    resources :group_members
+
+    root 'user_sessions#new'
   end
-
-  root 'user_sessions#new'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
